c47b188f7471d0a90612c3c8a7c20382dbe7eded
Marcin Kalita <marcin.kalita90@gmail.com>
2020-06-21 11:44:45 +0200

    Editor-pages-scss added:

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,20 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+
+const entryInput = document.querySelector ('.header__textaera');
+const button = document.querySelector ('.header__button-save');
+const loadButton = document.querySelector ('.header__button-load');
+
+button.addEventListener ('click', () => {
+    localStorage.setItem ('entry', entryInput.value);
+})
+
+const myResult = localStorage.getItem('entry');
+
+console.log(myResult);
+
+loadButton.addEventListener ('click', () => {
+    const loadButton = ('entry');
+})
